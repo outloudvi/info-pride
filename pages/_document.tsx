6d7028fba02dc57c6de6d4509aedf678e4eadddb
Outvi V <git@outv.im>
2022-03-13 17:10:38 +0800

    feat: license annotation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import Box from '@mui/material/Box'
 import { Html, Head, Main, NextScript } from 'next/document'
 import AppBar from '../components/AppBar'
 
@@ -18,6 +19,38 @@ export default function Document() {
         <AppBar />
         <Main />
         <NextScript />
+        <footer>
+          <Box
+            sx={{
+              textAlign: 'center',
+              color: '#666',
+            }}
+          >
+            <a
+              href="https://github.com/outloudvi/info-pride"
+              target="_blank"
+              rel="noopener"
+            >
+              源代码
+            </a>
+            <br />
+            来自{' '}
+            <a
+              href="https://wiki.biligame.com/idolypride/%E9%A6%96%E9%A1%B5"
+              target="_blank"
+              rel="noopener"
+            >
+              bwiki
+            </a>{' '}
+            的内容以{' '}
+            <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.zh">
+              CC BY-NC-SA 3.0
+            </a>{' '}
+            协议授权
+            <br />
+            来自游戏的内容 &copy; QualiArts 及相关实体
+          </Box>
+        </footer>
       </body>
     </Html>
   )
