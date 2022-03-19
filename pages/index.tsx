416451990d2e320a41822220e37be833d60b8fa9
Outvi V <git@outv.im>
2022-03-19 21:47:48 +0800

    feat(index): update links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
+import Link from 'next/link'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
-import CurrentEvents from '../components/CurrentEvents'
 import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
-import Link from 'next/link'
-import Layout from '../components/Layout'
 
-const MainPageSiteData = {
-  官方网站: 'https://idolypride.jp',
-  '攻略 wiki (BWiki)': 'https://wiki.biligame.com/idolypride/',
-  '攻略 wiki (AppMedia)': 'https://appmedia.jp/idolypride',
-  情报站微博: 'https://weibo.com/7326542616/',
-  'Telegram 讨论群组': 'https://t.me/hayasaka_mei',
-}
+import CurrentEvents from '../components/CurrentEvents'
+import Layout from '../components/Layout'
+import Paths from '../utils/paths'
+
+const MainPageSiteData = [
+  { 官方网站: 'https://idolypride.jp' },
+  { 情报站微博: 'https://weibo.com/7326542616/' },
+  {
+    '攻略 wiki (BWiki)': 'https://wiki.biligame.com/idolypride/',
+    '攻略 wiki (AppMedia)': 'https://appmedia.jp/idolypride',
+  },
+  {
+    'Telegram 群组': 'https://t.me/hayasaka_mei',
+    'QQ 群组': Paths.wiki('更多群组'),
+  },
+]
 
 const Pages = {
   剧情: '/stories',
@@ -24,7 +31,7 @@ const Home = () => {
   return (
     <Layout>
       <Grid container spacing={2} className="mt-3">
-        <Grid item xs={12} lg={4}>
+        <Grid item xs={12} lg={6}>
           <Typography textAlign="center" variant="h2">
             INFO PRIDE
           </Typography>
@@ -34,21 +41,26 @@ const Home = () => {
             justifyContent="center"
             paddingTop="2rem"
           >
-            {Object.entries(MainPageSiteData).map(([title, link], key) => (
-              <Button
-                key={key}
-                variant="outlined"
-                component="a"
-                href={link}
-                target="_blank"
-                rel="noopener"
-              >
-                {title}
-              </Button>
+            {MainPageSiteData.map((items, _key) => (
+              <Stack key={_key} direction="row">
+                {Object.entries(items).map(([title, link], key) => (
+                  <Button
+                    key={key}
+                    variant="outlined"
+                    component="a"
+                    href={link}
+                    target="_blank"
+                    rel="noopener"
+                    className="basis-0 grow mx-2"
+                  >
+                    {title}
+                  </Button>
+                ))}
+              </Stack>
             ))}
           </Stack>
         </Grid>
-        <Grid item xs={12} lg={8}>
+        <Grid item xs={12} lg={6}>
           <CurrentEvents />
         </Grid>
         {/* Line 2 */}
