10d657d669de7e90e66d2812e196071bd3d31774
Outvi V <git@outv.im>
2022-04-20 00:19:32 +0800

    feat(/notemap): migrate to Mantine

--- pages/notemap.tsx
+++ pages/notemap.tsx
@@ -1,13 +1,6 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import dynamic from 'next/dynamic'
-import Typography from '@mui/material/Typography'
-import FormControl from '@mui/material/FormControl'
-import InputLabel from '@mui/material/InputLabel'
-import Select from '@mui/material/Select'
-import MenuItem from '@mui/material/MenuItem'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-
+import { Grid, NativeSelect } from '@mantine/core'
 import _range from 'lodash/range'
 
 import Layout from '../components/Layout'
@@ -33,6 +26,10 @@ const NotemapPage = () => {
   const [notemap, setNotemap] = useState<string>(notemapList[0])
   const selectedNotemap = song && notemap ? Notemap[song][notemap] : undefined
 
+  useEffect(() => {
+    setNotemap(notemapList[0])
+  }, [song])
+
   const [laneColors, setLaneColors] = useState<string[]>([
     'blue',
     'blue',
@@ -43,86 +40,61 @@ const NotemapPage = () => {
 
   return (
     <Layout>
-      <Typography variant="h2">谱面</Typography>
-      <Grid container spacing={2} className="my-3">
-        <Grid item xs={12} lg={6}>
-          <Box>
-            <FormControl fullWidth className="mb-3">
-              <InputLabel id="lSong">曲目</InputLabel>
-              <Select
-                labelId="lSong"
-                value={song}
-                label="系列"
-                onChange={(i) => {
-                  setSong(i.target.value)
-                  setNotemap('')
-                }}
-              >
-                {songList.map((item, key) => (
-                  <MenuItem key={key} value={item}>
-                    <span
-                      dangerouslySetInnerHTML={{
-                        __html: songTitleList[item],
-                      }}
-                    ></span>
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl fullWidth>
-              <InputLabel id="lNotemap">谱面</InputLabel>
-              <Select
-                labelId="lNotemap"
-                value={notemap}
-                label="谱面"
-                onChange={(i) => {
-                  setNotemap(i.target.value)
-                }}
-              >
-                {notemapList.map((item, key) => (
-                  <MenuItem key={key} value={item}>
-                    {item}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Box>
-          <Box className="mt-4 flex justify-around">
+      <h2>谱面</h2>
+      <Grid gutter={20} className="my-3">
+        <Grid.Col xs={12} lg={6}>
+          <div>
+            <NativeSelect
+              label="曲目"
+              data={songList.map((item) => ({
+                label: songTitleList[item],
+                value: item,
+              }))}
+              value={song}
+              onChange={(i) => {
+                setSong(i.target.value)
+              }}
+              required
+            />
+            <NativeSelect
+              label="谱面"
+              data={notemapList}
+              value={notemap}
+              onChange={(i) => {
+                setNotemap(i.target.value)
+              }}
+              required
+            />
+          </div>
+          <div className="mt-4 flex justify-around">
             {_range(0, 5).map((val, key) => {
               const uiNumber = val + 1
               return (
-                <FormControl key={key}>
-                  <InputLabel id={`lColColor${uiNumber}`}>
-                    颜色{uiNumber}
-                  </InputLabel>
-                  <Select
-                    labelId={`lColColor${uiNumber}`}
-                    value={laneColors[val]}
-                    label={`颜色${uiNumber}`}
-                    onChange={(i) => {
-                      setLaneColors([
-                        ...laneColors.slice(0, val),
-                        i.target.value,
-                        ...laneColors.slice(val + 1),
-                      ])
-                    }}
-                  >
-                    {Object.entries(Colors).map(([name, value], key) => (
-                      <MenuItem key={key} value={value}>
-                        {name}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
+                <NativeSelect
+                  key={key}
+                  label={`颜色${uiNumber}`}
+                  value={laneColors[val]}
+                  data={Object.entries(Colors).map(([label, value]) => ({
+                    label,
+                    value,
+                  }))}
+                  onChange={(i) => {
+                    setLaneColors([
+                      ...laneColors.slice(0, val),
+                      i.target.value,
+                      ...laneColors.slice(val + 1),
+                    ])
+                  }}
+                />
               )
             })}
-          </Box>
-        </Grid>
-        <Grid item xs={12} lg={6}>
+          </div>
+        </Grid.Col>
+        <Grid.Col xs={12} lg={6}>
           {selectedNotemap !== undefined && (
             <NotemapGraph notemap={selectedNotemap} laneColors={laneColors} />
           )}
-        </Grid>
+        </Grid.Col>
       </Grid>
     </Layout>
   )
