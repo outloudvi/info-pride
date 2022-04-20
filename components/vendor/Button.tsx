e2cfaeb33948b62580d732d64a0abeb9152bf0d5
Outvi V <git@outv.im>
2022-04-20 16:52:55 +0800

    feat(Button): fix styles

--- components/vendor/Button.tsx
+++ components/vendor/Button.tsx
@@ -3,18 +3,27 @@ import { Button, ButtonProps } from '@mantine/core'
 // This is to replace the behavior of :hover and :active of <Button />
 const CustomizedButton = (props: ButtonProps<'button'>) => {
   const children = props.children
-  const color = props.color || 'blue'
+  const color = props.color ?? 'blue'
+  const variant = props.variant ?? 'default'
   return (
     <Button
       {...props}
       sx={(theme) => {
         return {
           '&:not(:disabled):hover': {
-            backgroundColor: theme.colors[color][4],
+            ...(variant === 'default' && {
+              backgroundColor: theme.colors[color][4],
+            }),
           },
           '&:not(:disabled):active': {
             transform: 'none',
-            backgroundColor: theme.colors[color][8],
+            ...(variant === 'default'
+              ? {
+                  backgroundColor: theme.colors[color][8],
+                }
+              : {
+                  backgroundColor: theme.colors[color][2],
+                }),
           },
         }
       }}
