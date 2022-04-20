20b92f165dcb9e68c100bce0f0f7e3a22bd963fc
Outvi V <git@outv.im>
2022-04-20 17:27:02 +0800

    feat(vendorx/ActionIcon): respect color

--- components/vendorx/mantine/ActionIcon.tsx
+++ components/vendorx/mantine/ActionIcon.tsx
@@ -3,6 +3,7 @@ import { ActionIcon, ActionIconProps } from '@mantine/core'
 // This is to replace the behavior of :hover and :active of <ActionIcon />
 const CustomXActionIcon = (props: ActionIconProps<'button'>) => {
   const children = props.children
+  const color = props.color ?? 'gray'
   const variant = props.variant ?? 'default'
   return (
     <ActionIcon
@@ -11,17 +12,17 @@ const CustomXActionIcon = (props: ActionIconProps<'button'>) => {
         return {
           '&:not(:disabled):hover': {
             ...(variant === 'default' && {
-              backgroundColor: theme.colors.gray[2],
+              backgroundColor: theme.colors[color][2],
             }),
           },
           '&:not(:disabled):active': {
             transform: 'none',
             ...(variant === 'default'
               ? {
-                  backgroundColor: theme.colors.gray[8],
+                  backgroundColor: theme.colors[color][8],
                 }
               : {
-                  backgroundColor: theme.colors.gray[2],
+                  backgroundColor: theme.colors[color][2],
                 }),
           },
         }
