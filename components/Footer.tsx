4481adef701c6b28e4d138ffa4dc22076e3c8ab8
Outvi V <git@outv.im>
2022-04-20 17:30:07 +0800

    feat(Footer): remove @mui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,37 +1,28 @@
-import Box from '@mui/material/Box'
-
 const Footer = () => (
-  <footer>
-    <Box
-      sx={{
-        color: '#666',
-      }}
-      className="text-center"
+  <footer className="text-center text-gray-500">
+    <a
+      href="https://github.com/outloudvi/info-pride"
+      target="_blank"
+      rel="noopener"
     >
-      <a
-        href="https://github.com/outloudvi/info-pride"
-        target="_blank"
-        rel="noopener"
-      >
-        源代码
-      </a>
-      <br />
-      来自{' '}
-      <a
-        href="https://wiki.biligame.com/idolypride/%E9%A6%96%E9%A1%B5"
-        target="_blank"
-        rel="noopener"
-      >
-        bwiki
-      </a>{' '}
-      的内容以{' '}
-      <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.zh">
-        CC BY-NC-SA 3.0
-      </a>{' '}
-      协议授权
-      <br />
-      来自游戏的内容 &copy; QualiArts 及相关实体
-    </Box>
+      源代码
+    </a>
+    <br />
+    来自{' '}
+    <a
+      href="https://wiki.biligame.com/idolypride/%E9%A6%96%E9%A1%B5"
+      target="_blank"
+      rel="noopener"
+    >
+      bwiki
+    </a>{' '}
+    的内容以{' '}
+    <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.zh">
+      CC BY-NC-SA 3.0
+    </a>{' '}
+    协议授权
+    <br />
+    来自游戏的内容 &copy; QualiArts 及相关实体
   </footer>
 )
 
