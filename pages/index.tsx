fb64556466f6d896b8624ff96e6388ed9119aac1
Outvi V <git@outv.im>
2022-06-01 00:18:51 +0800

    feat(index): somehow show git commit up there...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
-import { Button, Grid, Group, Stack } from '@mantine/core'
+import { execSync } from 'node:child_process'
+
+import { Badge, Button, Grid, Group, Stack } from '@mantine/core'
+import Link from 'next/link'
 
 import CurrentEvents from '#components/indexPage/CurrentEvents'
 import Notice from '#components/indexPage/Notice'
@@ -24,11 +27,21 @@ const MainPageSiteData = [
   },
 ]
 
-const Home = () => {
+const Home = ({ gitCommit }: { gitCommit: string }) => {
   return (
     <Grid className="mt-3">
       <Grid.Col xs={12} lg={6}>
-        <div className="text-center text-6xl mt-4">INFO PRIDE</div>
+        <div className="text-center text-6xl mt-4">
+          INFO PRIDE{' '}
+          <Link
+            href={`https://github.com/outloudvi/info-pride/tree/${gitCommit}`}
+            passHref
+          >
+            <Badge className="lowercase cursor-pointer align-super">
+              {gitCommit.slice(0, 8)}
+            </Badge>
+          </Link>
+        </div>
         <Stack spacing={15} justify="center" className="mt-2">
           {MainPageSiteData.map((items, _key) => (
             <Group key={_key}>
@@ -63,6 +76,15 @@ const Home = () => {
       </Grid.Col>
     </Grid>
   )
+}
+
+export async function getStaticProps() {
+  const gitCommit = execSync('git rev-parse HEAD').toString().trim()
+  return {
+    props: {
+      gitCommit,
+    },
+  }
 }
 
 export default Home
