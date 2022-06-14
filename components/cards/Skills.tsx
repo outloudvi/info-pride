f6a8a3d9e487fd768870ee67a5c360e40026803e
Outvi V <git@outv.im>
2022-06-14 22:53:05 +0800

    feat(Skills): show skill icons (partial)

--- components/cards/Skills.tsx
+++ components/cards/Skills.tsx
@@ -1,11 +1,73 @@
 import { useState } from 'react'
-import { Grid, Slider } from '@mantine/core'
+import { Slider, Stack } from '@mantine/core'
 import type { Skill } from 'hoshimi-types/ProtoMaster'
 import { SkillCategoryType } from 'hoshimi-types/ProtoEnum'
 import { useTranslation } from 'next-i18next'
 
 import type { Card as WikiCard } from '#data/wikiPages/cards'
 import AssetImage from '#components/AssetImage'
+import Paths from '#utils/paths'
+
+// really really chaotic
+// TODO: background
+const SkillImage = ({ skill }: { skill: Skill }) => {
+  if (skill.assetId) {
+    return (
+      <AssetImage
+        height={64}
+        name={`img_icon_skill_${skill.assetId}`}
+        ratio={1}
+        alt={'Skill icon'}
+        className="bg-black"
+      />
+    )
+  }
+
+  const skillIcons = skill.levels[0].skillDetails.map(
+    (x) =>
+      'img_icon_skill-normal_' + x.efficacyId.split('-')[1].replace(/_/g, '-')
+  )
+
+  if (skillIcons.length === 1) {
+    return (
+      <AssetImage
+        height={64}
+        name={skillIcons[0]}
+        ratio={1}
+        alt={'Skill icon'}
+        className="bg-black absolute -mt-1 -mr-1"
+      />
+    )
+  }
+
+  if (skillIcons.length === 2) {
+    // https://wiki.biligame.com/idolypride/模板:技能图标
+    return (
+      <div className="h-16 w-16 relative bg-black">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={Paths.assets(skillIcons[1])}
+          loading="lazy"
+          height={54.4}
+          width={54.4}
+          className="absolute left-0 bottom-0 -ml-1 -mb-1"
+          alt="Primary component of the skill icon"
+        />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={Paths.assets(skillIcons[0])}
+          loading="lazy"
+          height={32}
+          width={32}
+          className="absolute top-0 right-0"
+          alt="Secondary component of the skill icon"
+        />
+      </div>
+    )
+  }
+
+  return <></>
+}
 
 const Skill = ({
   skill,
@@ -61,11 +123,13 @@ const Skills = ({
   wikiCardData?: WikiCard
 }) => {
   return (
-    <Grid className="max-w-full">
+    <Stack>
       {skills.map((skill, key) => (
-        <Grid.Col span={4} key={key}>
+        <div key={key} className="flex items-center">
+          <div className="mr-2">
+            <SkillImage skill={skill} />
+          </div>
           <Skill
-            className="grow"
             key={key}
             skill={skill}
             titleCn={
@@ -85,9 +149,9 @@ const Skills = ({
               ] as string | undefined
             }
           />
-        </Grid.Col>
+        </div>
       ))}
-    </Grid>
+    </Stack>
   )
 }
 
