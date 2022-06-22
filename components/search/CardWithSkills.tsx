9c4025afcbc856bd112ffeb3425419a7e0e63c44
Outvi V <git@outv.im>
2022-06-22 12:24:20 +0800

    feat: display wiki page on /search

--- components/search/CardWithSkills.tsx
+++ components/search/CardWithSkills.tsx
@@ -8,9 +8,11 @@ import dayjsTz from 'dayjs/plugin/timezone'
 
 import SkillDesc from './SkillDesc'
 
+import CCIDTable from '#data/ccid'
 import { APIResponseOf } from '#utils/api'
 import getCardColor from '#utils/getCardColor'
 import { CharacterChineseNameList, CharacterId } from '#data/vendor/characterId'
+import Paths from '#utils/paths'
 
 dayjs.extend(dayjsUtc)
 dayjs.extend(dayjsTz)
@@ -34,15 +36,35 @@ const CardWithSkills = ({
     .tz('Asia/Tokyo')
     .format('YYYY-MM-DD')
 
+  const cardCcid = CCIDTable?.[card.characterId as CharacterId]?.find(
+    (x) => x.cardId === card.id
+  )
+
   return (
     <Card className="bg-neutral-200 dark:bg-neutral-800 rounded-md mb-2">
       <b>{name}</b>
+      {cardCcid && (
+        <>
+          <br className="lg:hidden" />
+          <a
+            className="lg:float-right"
+            href={Paths.wiki(
+              `${CharacterChineseNameList[characterId as CharacterId]}/卡牌/${
+                cardCcid.ccid
+              }`
+            )}
+          >
+            Wiki 页面
+          </a>
+        </>
+      )}
       <br />
       <span>
         {CharacterChineseNameList[characterId as CharacterId]} /{' '}
         {$v(CardType[type])} / {$v(cardColor)} / {initialRarity}★ / 发布于{' '}
         {releaseDateFmt}
       </span>
+
       <Grid className="mt-1">
         {skillData.map((skill, index) => (
           <Grid.Col
