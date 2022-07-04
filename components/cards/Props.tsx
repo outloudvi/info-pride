568630055510037d6a753b86c024ad7bb43419d1
Outvi V <git@outv.im>
2022-07-04 22:02:30 +0800

    feat(cards/Props): add icons and total score

--- components/cards/Props.tsx
+++ components/cards/Props.tsx
@@ -1,7 +1,9 @@
-import { Skeleton } from '@mantine/core'
+/* eslint-disable @next/next/no-img-element */
+import { Grid, Skeleton } from '@mantine/core'
 import type { Card, CardRarity } from 'hoshimi-types/ProtoMaster'
 
 import useApi from '#utils/useApi'
+import Paths from '#utils/paths'
 
 const Props = ({
   level,
@@ -50,31 +52,77 @@ const Props = ({
       1000
   )
 
+  const mental = 100
+  const critical = 100
+
+  // See also: https://wiki.biligame.com/idolypride/模块:数值计算
+  const totalScore =
+    Math.floor(calc(vocalRatioPermil) * 0.5) +
+    Math.floor(calc(danceRatioPermil) * 0.5) +
+    Math.floor(calc(visualRatioPermil) * 0.5) +
+    Math.floor(stamina * 0.8) +
+    mental * 2 +
+    critical * 3
+
+  const cardColorClassName =
+    vocalRatioPermil === valueAtMaxType
+      ? 'text-vocal'
+      : danceRatioPermil === valueAtMaxType
+      ? 'text-dance'
+      : visualRatioPermil === valueAtMaxType
+      ? 'text-visual'
+      : ''
+
   return (
-    <div>
-      <span className="mr-3 text-vocal">
-        {vocalRatioPermil === valueAtMaxType ? (
-          <b>{calc(vocalRatioPermil)}</b>
-        ) : (
-          <span>{calc(vocalRatioPermil)}</span>
-        )}
-      </span>
-      <span className="mr-3 text-dance">
-        {danceRatioPermil === valueAtMaxType ? (
-          <b>{calc(danceRatioPermil)}</b>
-        ) : (
-          <span>{calc(danceRatioPermil)}</span>
-        )}
-      </span>
-      <span className="mr-3 text-visual">
-        {visualRatioPermil === valueAtMaxType ? (
-          <b>{calc(visualRatioPermil)}</b>
-        ) : (
-          <span>{calc(visualRatioPermil)}</span>
-        )}
-      </span>
-      <span className="mr-3 text-stamina">{stamina}</span>
-    </div>
+    <Grid className="text-lg">
+      <Grid.Col span={6} className="flex justify-center items-center">
+        <div className={`${cardColorClassName} text-[1.5em]`}>{totalScore}</div>
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <Grid>
+          <Grid.Col span={6}>
+            <div className="text-vocal text-center">
+              <img
+                src={Paths.sprite('x-icon_parameter_vocal')}
+                alt="Vocal parameter icon"
+                height={25}
+                className="mr-2"
+              />
+              {calc(vocalRatioPermil)}
+            </div>
+            <div className="text-visual text-center">
+              <img
+                src={Paths.sprite('x-icon_parameter_visual')}
+                alt="Vocal parameter icon"
+                height={25}
+                className="mr-2"
+              />
+              {calc(visualRatioPermil)}
+            </div>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <div className="text-dance text-center">
+              <img
+                src={Paths.sprite('x-icon_parameter_dance')}
+                alt="Vocal parameter icon"
+                height={25}
+                className="mr-2"
+              />
+              {calc(danceRatioPermil)}
+            </div>
+            <div className="text-stamina text-center">
+              <img
+                src={Paths.sprite('x-icon_parameter_stamina')}
+                alt="Vocal parameter icon"
+                height={25}
+                className="mr-2"
+              />
+              {stamina}
+            </div>
+          </Grid.Col>
+        </Grid>
+      </Grid.Col>
+    </Grid>
   )
 }
 
