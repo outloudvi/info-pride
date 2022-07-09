de64a243d48ee0196f9e6c573390929b8853eb9a
Outvi V <git@outv.im>
2022-07-09 18:12:14 +0800

    ci: fix mjs/cjs conversion

---
+++ scripts/to-esm.mjs
@@ -0,0 +1,41 @@
+import * as fs from 'node:fs'
+import { dirname, join } from 'node:path'
+
+const __dirname = dirname(new URL(import.meta.url).pathname)
+
+function writeTo(obj, file) {
+  const pak = JSON.parse(fs.readFileSync(file, 'utf-8'))
+  fs.writeFileSync(
+    file,
+    JSON.stringify({
+      ...pak,
+      ...obj,
+    })
+  )
+}
+
+const target = process.argv?.[2] ?? 'mjs'
+
+if (target === 'cjs') {
+  writeTo(
+    {
+      type: undefined,
+    },
+    join(__dirname, '../package.json')
+  )
+} else if (target === 'mjs') {
+  writeTo(
+    {
+      type: 'module',
+    },
+    join(__dirname, '../package.json')
+  )
+  writeTo(
+    {
+      'ts-node': {
+        esm: true,
+      },
+    },
+    join(__dirname, '../tsconfig.json')
+  )
+}
