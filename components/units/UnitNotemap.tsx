db930fad167454e5b910c34e372d981e83203a20
Outvi V <git@outv.im>
2022-09-01 00:07:06 +0800

    feat(UnitNotemap): give spinner on request load

--- components/units/UnitNotemap.tsx
+++ components/units/UnitNotemap.tsx
@@ -1,4 +1,5 @@
 import { MusicChart } from 'hoshimi-types/types'
+import { Loader } from '@mantine/core'
 
 import { CardTiny } from './types'
 
@@ -36,32 +37,42 @@ const UnitNotemap = ({
         })
     }
 
-    const { data: skillResult1 } = useSkillRunner(1)
-    const { data: skillResult2 } = useSkillRunner(2)
-    const { data: skillResult3 } = useSkillRunner(3)
-    const { data: skillResult4 } = useSkillRunner(4)
-    const { data: skillResult5 } = useSkillRunner(5)
+    const { data: skillResult1, isSuccess: srState1 } = useSkillRunner(1)
+    const { data: skillResult2, isSuccess: srState2 } = useSkillRunner(2)
+    const { data: skillResult3, isSuccess: srState3 } = useSkillRunner(3)
+    const { data: skillResult4, isSuccess: srState4 } = useSkillRunner(4)
+    const { data: skillResult5, isSuccess: srState5 } = useSkillRunner(5)
+    const skillRunnerOk =
+        srState1 && srState2 && srState3 && srState4 && srState5
 
     return (
-        <NotemapView
-            chart={chart}
-            // TODO: changable lane colors
-            laneColors={laneColors}
-            landingSkillChart={{
-                1: skillResult1 ?? [],
-                2: skillResult2 ?? [],
-                3: skillResult3 ?? [],
-                4: skillResult4 ?? [],
-                5: skillResult5 ?? [],
-            }}
-            imageChart={{
-                1: cardTinyToAssetUrl(unitCards[1]),
-                2: cardTinyToAssetUrl(unitCards[2]),
-                3: cardTinyToAssetUrl(unitCards[3]),
-                4: cardTinyToAssetUrl(unitCards[4]),
-                5: cardTinyToAssetUrl(unitCards[5]),
-            }}
-        />
+        <div>
+            {!skillRunnerOk && (
+                <div className="flex justify-center items-center mb-2">
+                    <Loader />
+                    <span className="ml-2">正在运行技能有效性计算...</span>
+                </div>
+            )}
+            <NotemapView
+                chart={chart}
+                // TODO: changable lane colors
+                laneColors={laneColors}
+                landingSkillChart={{
+                    1: skillResult1 ?? [],
+                    2: skillResult2 ?? [],
+                    3: skillResult3 ?? [],
+                    4: skillResult4 ?? [],
+                    5: skillResult5 ?? [],
+                }}
+                imageChart={{
+                    1: cardTinyToAssetUrl(unitCards[1]),
+                    2: cardTinyToAssetUrl(unitCards[2]),
+                    3: cardTinyToAssetUrl(unitCards[3]),
+                    4: cardTinyToAssetUrl(unitCards[4]),
+                    5: cardTinyToAssetUrl(unitCards[5]),
+                }}
+            />
+        </div>
     )
 }
 
