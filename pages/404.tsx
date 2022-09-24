6f059467b757aa605be8000dd2df71e4c85ada24
Outvi V <git@outv.im>
2022-09-24 21:48:43 +0800

    i18n: /404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,24 @@
 import Link from 'next/link'
+import { useTranslations } from 'next-intl'
 
 import getI18nProps from '#utils/getI18nProps'
 
 export default function Custom404() {
+    const $t = useTranslations('404')
     return (
         <div className="text-center">
-            <h1>404 - Page Not Found</h1>
+            <h1>404 - {$t('title')}</h1>
             <p>
-                <Link href="/">Get back to main page.</Link>
+                <Link href="/">{$t('back_to_main_page')}</Link>
             </p>
             <p>
                 INFO PRIDE |{' '}
                 <a href="https://github.com/outloudvi/info-pride/issues">
-                    Report bugs
+                    {$t('bug_report')}
                 </a>
             </p>
         </div>
     )
 }
 
-export const getStaticProps = getI18nProps()
+export const getStaticProps = getI18nProps(['404'])
