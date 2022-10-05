f8cd3e88309d42db9c7048c9d007b85d087fb222
Outvi V <git@outv.im>
2022-10-06 00:32:26 +0800

    feat: en eventstories sample

--- components/ExternalVideo.ts
+++ components/ExternalVideo.ts
@@ -1,17 +1,20 @@
-type ExternalBilibiliVideo = {
-    type: 'bilibili'
+export type ExternalVideo = {
+    type: 'bilibili' | 'youtube'
     vid: string
+    // Bilibili only
     pid?: number
 }
 
-export type ExternalVideo = ExternalBilibiliVideo
-
-export function toVideoLink(e: ExternalVideo) {
+export function toVideoLink(e: ExternalVideo): string {
     switch (e.type) {
         case 'bilibili': {
             return `https://www.bilibili.com/video/${e.vid}${
                 e.pid ? `?p=${e.pid}` : ''
             }`
         }
+        case 'youtube': {
+            return `https://www.youtube.com/watch?v=${e.vid}`
+        }
     }
+    return ''
 }
