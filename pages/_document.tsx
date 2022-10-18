deecd44b4fe7a7653b3ec2f96ae31f922cab22db
Outvi V <git@outv.im>
2022-10-19 01:02:47 +0800

    feat: social preview image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,9 +19,10 @@ const allowedConnectionDomains = [
     'res.cloudinary.com',
 ]
 
-const META_TITLE = 'INFO PRIDE'
+const META_TITLE = 'info pride'
 const META_DESCRIPTION =
     'An informational website for Project IDOLY PRIDE fans.'
+const BASEURL = 'https://ip.outv.im'
 
 const cspRules = (scriptOthers: string) =>
     [
@@ -57,11 +58,8 @@ export default class _Document extends Document {
                     <meta httpEquiv="Content-Security-Policy" content={csp} />
                     <meta property="og:title" content={META_TITLE} />
                     <meta property="twitter:title" content={META_TITLE} />
-                    <meta property="og:url" content="https://ip.outv.im/" />
-                    <meta
-                        property="twitter:url"
-                        content="https://ip.outv.im/"
-                    />
+                    <meta property="og:url" content={BASEURL} />
+                    <meta property="twitter:url" content={BASEURL} />
                     <meta name="description" content={META_DESCRIPTION} />
                     <meta
                         property="og:description"
@@ -71,6 +69,14 @@ export default class _Document extends Document {
                         property="twitter:description"
                         content={META_DESCRIPTION}
                     />
+                    <meta
+                        property="og:image"
+                        content={BASEURL + '/social.png'}
+                    />
+                    <meta property="og:image:alt" content={META_DESCRIPTION} />
+                    <meta property="og:image:width" content="1200" />
+                    <meta property="og:image:height" content="600" />
+                    <meta property="og:site_name" content={META_TITLE} />
                     <meta property="og:type" content="website" />
                 </Head>
                 <body>
