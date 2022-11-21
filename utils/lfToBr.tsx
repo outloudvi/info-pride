eae18834025b4f8377d3de798b104c5256fa7aed
Outvi V <git@outv.im>
2022-11-22 00:07:50 +0800

    fix(lfToBr): add key to items

--- utils/lfToBr.tsx
+++ utils/lfToBr.tsx
@@ -2,9 +2,9 @@ export default function lfToBr(str: string): JSX.Element {
     const split = str.split('\n')
     const ret = []
     for (let i = 0; i < split.length; i++) {
-        ret.push(split[i])
+        ret.push(<span key={i}>{split[i]}</span>)
         if (i + 1 !== split.length) {
-            ret.push(<br />)
+            ret.push(<br key={`b${i}`} />)
         }
     }
     return <>{ret}</>
