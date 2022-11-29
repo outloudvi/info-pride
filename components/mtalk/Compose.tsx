4c20f8d173d20c3ca6b38df68249d547a16b175f
Outvi V <git@outv.im>
2022-11-29 23:37:48 +0800

    feat(Compose): insert stamps

--- components/mtalk/Compose.tsx
+++ components/mtalk/Compose.tsx
@@ -1,14 +1,19 @@
 import type { Dispatch, SetStateAction } from 'react'
 import { useState } from 'react'
-import { ActionIcon, Group, Textarea, TextInput } from '@mantine/core'
+import { ActionIcon, Group, Modal, Textarea } from '@mantine/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
+    faCirclePlus,
     faEllipsisVertical,
     faPaperPlane,
 } from '@fortawesome/free-solid-svg-icons'
+import { useTranslations } from 'next-intl'
 
 import type { CharacterIdWithManager, CommuLine, EditorPref } from './types'
 import CharacterIcon from './CharacterIcon'
+
+import Stamps from '#data/stamps'
+import AssetImage from '#components/AssetImage'
 
 const Compose = ({
     currChrId,
@@ -19,7 +24,10 @@ const Compose = ({
     setCommuData: Dispatch<SetStateAction<CommuLine[]>>
     setPref: Dispatch<SetStateAction<EditorPref>>
 }) => {
+    const $t = useTranslations('mtalk')
+
     const [text, setText] = useState('')
+    const [insertStampModal, setInsertStampModal] = useState(false)
 
     const submit = () => {
         if (text.trim() === '') return
@@ -34,28 +42,67 @@ const Compose = ({
     }
 
     return (
-        <Group className="p-2 min-h-[32px] max-h-[128px]">
-            <ActionIcon
-                variant="outline"
-                onClick={() => {
-                    setPref((x) => ({
-                        ...x,
-                        expandMenu: !x.expandMenu,
-                    }))
-                }}
+        <>
+            <Modal
+                opened={insertStampModal}
+                onClose={() => setInsertStampModal(false)}
+                title={$t('Insert a stamp')}
+                size="65%"
             >
-                <FontAwesomeIcon icon={faEllipsisVertical} color="white" />
-            </ActionIcon>
-            <CharacterIcon id={currChrId} />
-            <Textarea
-                className="flex-grow min-h-[32px] max-h-[128px]"
-                value={text}
-                onChange={(e) => setText(e.currentTarget.value)}
-            />
-            <ActionIcon variant="outline" onClick={submit}>
-                <FontAwesomeIcon icon={faPaperPlane} color="white" />
-            </ActionIcon>
-        </Group>
+                <Group>
+                    {Stamps.map((stampAssetId, key) => (
+                        <AssetImage
+                            key={key}
+                            name={`img_message_stamp_${stampAssetId}`}
+                            ratio={1}
+                            height="9rem"
+                            alt={`Stamp: ${stampAssetId}`}
+                            onClick={() => {
+                                setCommuData((x) => [
+                                    ...x,
+                                    {
+                                        characterId: currChrId,
+                                        stampAssetId,
+                                    },
+                                ])
+                                setInsertStampModal(false)
+                            }}
+                        />
+                    ))}
+                </Group>
+            </Modal>
+
+            <Group className="p-2 min-h-[32px] max-h-[128px]">
+                <ActionIcon
+                    variant="outline"
+                    onClick={() => {
+                        setPref((x) => ({
+                            ...x,
+                            expandMenu: !x.expandMenu,
+                        }))
+                    }}
+                >
+                    <FontAwesomeIcon icon={faEllipsisVertical} color="white" />
+                </ActionIcon>
+                <ActionIcon
+                    variant="outline"
+                    onClick={() => {
+                        setInsertStampModal(true)
+                    }}
+                >
+                    <FontAwesomeIcon icon={faCirclePlus} color="white" />
+                </ActionIcon>
+                <CharacterIcon id={currChrId} />
+                <Textarea
+                    className="flex-grow min-h-[32px] max-h-[128px]"
+                    value={text}
+                    onChange={(e) => setText(e.currentTarget.value)}
+                />
+                <ActionIcon variant="outline" onClick={submit}>
+                    <FontAwesomeIcon icon={faPaperPlane} color="white" />
+                </ActionIcon>
+            </Group>
+        </>
     )
 }
 
