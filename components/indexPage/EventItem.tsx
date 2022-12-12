f717b56fa47272aa4f66a6ea6c5c61096b615006
Outvi V <git@outv.im>
2022-12-13 00:23:59 +0800

    feat(EventItem): display local time

--- components/indexPage/EventItem.tsx
+++ components/indexPage/EventItem.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Group, ActionIcon, Progress } from '@mantine/core'
+import { Group, ActionIcon, Progress, Tooltip } from '@mantine/core'
 import { useTranslations } from 'next-intl'
 import dayjs from 'dayjs'
 import dayjsUtc from 'dayjs/plugin/utc'
@@ -14,21 +14,30 @@ import Paths from '#utils/paths'
 dayjs.extend(dayjsUtc)
 dayjs.extend(dayjsTz)
 
+const CommonDateFormat = 'YYYY/M/D'
+const ExtendedDateFormat = 'YYYY/M/D H:mm:ss'
+const SourceTimeZone = 'Asia/Tokyo'
+
 const EventItem = ({ item }: { item: EventItem }) => {
     const $t = useTranslations('index')
 
     const { title, type, start, end, link } = item
 
+    const startDate = dayjs(start).tz(SourceTimeZone)
+    const endDate = dayjs(end).tz(SourceTimeZone)
     const now = dayjs()
-    const startDateStr = dayjs(start).tz('Asia/Tokyo').format('YYYY/M/D')
+    const startDateStr = startDate.format(CommonDateFormat)
     const endDateStr = useMemo(() => {
-        const endDate = dayjs(end).tz('Asia/Tokyo')
         const startOfEndDate = endDate.startOf('day')
         if (endDate.diff(startOfEndDate, 'second') === 0) {
-            return endDate.subtract(1, 'day').format('YYYY/M/D')
+            return endDate.subtract(1, 'day').format(CommonDateFormat)
         }
-        return endDate.format('YYYY/M/D')
-    }, [end])
+        return endDate.format(CommonDateFormat)
+    }, [endDate])
+
+    const localTimeString = `${$t('local_time')} ${startDate.format(
+        ExtendedDateFormat
+    )} - ${endDate.format(ExtendedDateFormat)}`
 
     return (
         <div className="mb-4">
@@ -51,7 +60,12 @@ const EventItem = ({ item }: { item: EventItem }) => {
                 <div>
                     <span>{title}</span> <br />
                     <span className="text-sm text-gray-500">
-                        {$t(EventType[type])} / {startDateStr} - {endDateStr}
+                        {$t(EventType[type])} /{' '}
+                        <Tooltip label={localTimeString}>
+                            <span>
+                                {startDateStr} - {endDateStr} (JST)
+                            </span>
+                        </Tooltip>
                     </span>
                 </div>
             </Group>
