1079a7f72172684af82dcd406f44063314358f25
Outvi V <git@outv.im>
2022-12-17 15:18:54 +0800

    chore: refresh deps and add @sentry/replay

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs'
+import { Replay } from '@sentry/replay'
 
 import getSentryDSN from './sentryUtils'
 
@@ -10,8 +11,12 @@ Sentry.init({
     dsn: getSentryDSN(),
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 0.8,
-    // ...
-    // Note: if you want to override the automatic release value, do not set a
-    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-    // that it will also get attached to your source maps
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
+
+    integrations: [new Replay()],
 })
