338d400013988197a33a608b98a0b1f32ff865e4
Outvi V <git@outv.im>
2022-12-18 22:49:25 +0800

    feat(NoticeTop): 221218
    R.I.P. Sayaka Kanda

--- components/indexPage/NoticeTop.tsx
+++ components/indexPage/NoticeTop.tsx
@@ -1,10 +1,54 @@
+import { Alert } from '@mantine/core'
 import { useLocalStorage } from '@mantine/hooks'
 import { useTranslations } from 'next-intl'
 import { useEffect } from 'react'
 
+import lfToBr from '#utils/lfToBr'
+
 type NoticeUnit = (x: () => void, y: (s: string) => string) => JSX.Element
 
-const GlobalNotices: Record<string, NoticeUnit> = {}
+const GlobalNotices: Record<string, NoticeUnit> = {
+    '221218-kanda': (off, $t) => (
+        <Alert
+            color="gray"
+            title={$t('221218-title')}
+            withCloseButton
+            onClose={off}
+            closeButtonLabel={$t('btn_close')}
+        >
+            {lfToBr($t('221218-desc'))}
+            <p>
+                <a
+                    href="https://www.ponycanyon.co.jp/music/PCCA000006164"
+                    rel="noopener nofollow"
+                >
+                    {$t('221218-link')}
+                </a>{' '}
+                /{' '}
+                <a
+                    href="https://www.youtube.com/watch?v=-GQQW7fiujw"
+                    rel="noopener nofollow"
+                >
+                    {$t('221218-video')}
+                </a>{' '}
+                /{' '}
+                <a
+                    href="https://open.spotify.com/album/4b9yufB1czOPML40w0LUyc"
+                    rel="noopener nofollow"
+                >
+                    {$t('221218-spotify')}
+                </a>{' '}
+                /{' '}
+                <a
+                    href="https://music.apple.com/jp/album/liberty-memorial/1657121580"
+                    rel="noopener nofollow"
+                >
+                    {$t('221218-apple-music')}
+                </a>
+            </p>
+        </Alert>
+    ),
+}
 
 const NoticeItem = ({
     nKey,
