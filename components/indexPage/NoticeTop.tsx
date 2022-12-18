e525e9be58936e67940361d59c71562bae55dc28
Outvi V <git@outv.im>
2022-12-18 13:10:08 +0800

    chore(notice): remove 221105-mirai

--- components/indexPage/NoticeTop.tsx
+++ components/indexPage/NoticeTop.tsx
@@ -1,36 +1,10 @@
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Alert } from '@mantine/core'
 import { useLocalStorage } from '@mantine/hooks'
 import { useTranslations } from 'next-intl'
 import { useEffect } from 'react'
 
-import lfToBr from '#utils/lfToBr'
-
 type NoticeUnit = (x: () => void, y: (s: string) => string) => JSX.Element
 
-const GlobalNotices: Record<string, NoticeUnit> = {
-    '221105-mirai': (off, $t) => (
-        <Alert
-            icon={<FontAwesomeIcon icon={faInfoCircle} />}
-            color="cyan"
-            title={$t('221105-title')}
-            withCloseButton
-            onClose={off}
-            closeButtonLabel={$t('btn_close')}
-        >
-            {lfToBr($t('221105-desc'))}
-            <p>
-                <a
-                    href="https://idolypride.jp/news/2022/10/27/2127/"
-                    rel="noopener nofollow"
-                >
-                    {$t('221105-link')}
-                </a>
-            </p>
-        </Alert>
-    ),
-}
+const GlobalNotices: Record<string, NoticeUnit> = {}
 
 const NoticeItem = ({
     nKey,
@@ -67,6 +41,7 @@ const NoticeTop = () => {
         if (!ls) return
         ls.removeItem('220803-Notice-Closed')
         ls.removeItem('220914-boltrend')
+        ls.removeItem('221105-boltrend')
     }, [])
 
     return (
