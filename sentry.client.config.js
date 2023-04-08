2604968cbc89ec37c1b6298e9d119aaa566a8aa9
Outvi V <git@outv.im>
2023-04-08 22:35:03 +0800

    chore(sentry): reduce replay sampling rate

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -13,10 +13,10 @@ Sentry.init({
     tracesSampleRate: 0.8,
     // This sets the sample rate to be 10%. You may want this to be 100% while
     // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
+    replaysSessionSampleRate: process.env.VERCEL_ENV === 'production' ? 0 : 1,
     // If the entire session is not sampled, use the below sample rate to sample
     // sessions when an error occurs.
-    replaysOnErrorSampleRate: 1.0,
+    replaysOnErrorSampleRate: 0.3,
 
     integrations: [new Replay()],
 })
