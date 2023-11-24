195a4bcdf085b7fd8946e8344a3a497cca695136
Outvi V <19144373+outloudvi@users.noreply.github.com>
2023-11-24 18:16:52 +0800

    feat(IndexTitle): What is <del>Idol</d> Love Live?

--- components/indexPage/IndexTitle.tsx
+++ components/indexPage/IndexTitle.tsx
@@ -2,7 +2,7 @@ const IndexTitle = () => {
     return (
         <div className="text-2xl px-3 py-1 md:py-0 text-white bg-[#ff3cb4] rounded flex items-center justify-center">
             <b>
-                numazu <br className="hidden md:block" /> pride
+                Numazu <br className="hidden md:block" /> Pride!
             </b>
         </div>
     )
