cdd4a3fa187617ddb05a354ff77322b55c5f8399
Outvi V <git@outv.im>
2023-11-25 14:49:41 +0800

    fix(getI18nProps): somehow import() will give the wrong file...

--- utils/getI18nProps.ts
+++ utils/getI18nProps.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs'
+
 const getI18nProps =
     (sources: string[] = []) =>
     async ({ locale }: { locale: string }) => {
@@ -12,7 +14,10 @@ export const getI18nMessages = async (
     locale: string,
     source: string,
     key: string,
-) => import(`../locales/${locale}/${source}.json`).then((x) => x.default?.[key])
+) =>
+    JSON.parse(
+        fs.readFileSync(`./locales/${locale}/${source}.json`, 'utf-8'),
+    )?.[key]
 
 export const addI18nMessages = async (
     locale: string,
@@ -20,7 +25,9 @@ export const addI18nMessages = async (
 ) => {
     const _m: Record<string, Record<string, string>> = {}
     for (const i of [...sources, 'common']) {
-        _m[i] = (await import(`../locales/${locale}/${i}.json`)).default
+        _m[i] = JSON.parse(
+            fs.readFileSync(`./locales/${locale}/${i}.json`, 'utf-8'),
+        )
     }
     return {
         _m,
