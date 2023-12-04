d6b15bc97b16b6d3bf7aea7a749753301a95e574
Outvi V <git@outv.im>
2023-12-04 23:19:05 +0800

    chore: good bye, /pages

--- utils/expandModule.ts
+++
@@ -1,9 +0,0 @@
-export default function expandModule(
-    item: Record<string, any>,
-): Record<string, any> {
-    const result = {}
-    for (let [key, value] of Object.entries(item)) {
-        result[key] = value
-    }
-    return result
-}
