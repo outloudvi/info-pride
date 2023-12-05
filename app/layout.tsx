be6a58e586a580ff28fc2face3a0e5f898fcf673
Outvi V <git@outv.im>
2023-12-05 23:56:40 +0800

    fix: make not-found.tsx happy

---
+++ app/layout.tsx
@@ -0,0 +1,6 @@
+// well, we still need this due to `not-found.tsx`...
+const DummyRootLayout = ({ children }: { children: JSX.Element }) => {
+    return children
+}
+
+export default DummyRootLayout
