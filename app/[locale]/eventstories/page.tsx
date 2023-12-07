88fa283260917729dc1f5f75176d31d3c643d5e1
Outvi V <git@outv.im>
2023-12-08 00:15:11 +0800

    feat(eventstories): client i18n

--- app/[locale]/eventstories/page.tsx
+++ app/[locale]/eventstories/page.tsx
@@ -1,8 +1,8 @@
 import { getTranslations } from 'next-intl/server'
 
 import { fetchApi } from '#utils/fetchApi'
-import Title from '#components/Title'
 import EventStoriesMainView from '#components/eventstories/EventStoriesMainView'
+import { withAsyncMessages } from '#utils/withMessages'
 
 const EventStoriesPage = async () => {
     const $t = await getTranslations('eventstories')
@@ -11,11 +11,25 @@ const EventStoriesPage = async () => {
 
     return (
         <>
-            <Title title={$t('Event Stories')} />
+            <h2>{$t('Event Stories')}</h2>
             <p>{$t('stories_ordering')}</p>
             <EventStoriesMainView EventStoriesData={EventStoriesData} />
         </>
     )
 }
 
-export default EventStoriesPage
+export async function generateMetadata({
+    params: { locale },
+}: {
+    params: { locale: string }
+}) {
+    const $t = await getTranslations({ locale, namespace: 'eventstories' })
+    return {
+        title: $t('Event Stories'),
+    }
+}
+
+export default withAsyncMessages(EventStoriesPage, [
+    'eventstories',
+    'v-event-name',
+])
