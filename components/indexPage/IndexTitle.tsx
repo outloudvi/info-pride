ecad9a4c4df84fc738d416b776988a7972faaa22
Outvi V <git@outv.im>
2024-01-18 00:03:54 +0800

    chore: Revert "feat(indexPage): numazu pride"
    This reverts commit 45bf0f619f3cdb10c282dae440dec019cb22898f.

--- components/indexPage/IndexTitle.tsx
+++ components/indexPage/IndexTitle.tsx
@@ -1,8 +1,8 @@
 const IndexTitle = () => {
     return (
-        <div className="text-2xl px-3 py-1 md:py-0 text-white bg-[#ff3cb4] rounded flex items-center justify-center">
+        <div className="text-2xl px-3 py-1 md:py-0 text-white bg-[#1428ff] rounded flex items-center justify-center">
             <b>
-                Numazu <br className="hidden md:block" /> Pride!
+                info <br className="hidden md:block" /> pride
             </b>
         </div>
     )
