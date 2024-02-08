91b5d6fc63806db7c0f5c14ff4709438a5a5c80e
Outvi V <git@outv.im>
2024-02-08 23:25:09 +0800

    feat(photos): UX updates

--- components/photos/PhotoAioItem.tsx
+++ components/photos/PhotoAioItem.tsx
@@ -1,6 +1,6 @@
-import { Slider } from '@mantine/core'
+import { Grid, GridCol, Slider } from '@mantine/core'
 import { useTranslations } from 'next-intl'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import type { PhotoAioDetail } from 'hoshimi-types/types'
 
 import PhotoAbility from './PhotoAbility'
@@ -12,6 +12,13 @@ const PhotoAioItem = ({ data }: { data: PhotoAioDetail }) => {
     const $cs = useTranslations('cards_slug')
 
     const { name, assetId, rarity, abilities, level: initialLevel } = data
+    const maxLevel = useMemo(
+        () =>
+            data.abilities[0].photoAbilityLevels[
+                data.abilities[0].photoAbilityLevels.length - 1
+            ].level,
+        [data],
+    )
     const [level, setLevel] = useState(initialLevel)
 
     return (
@@ -27,23 +34,43 @@ const PhotoAioItem = ({ data }: { data: PhotoAioDetail }) => {
             </div>
             <h2 lang="ja">{name}</h2>
             <p>
-                {$cs('Rarity')}: {rarity}
+                {$cs('Rarity')}: {rarity} <br />
+                {$t('Initial level')}: {initialLevel}
             </p>
             <div className="clear-both" />
             <h3>{$t('Skills')}</h3>
-            <Slider
-                min={initialLevel}
-                max={
-                    data.abilities[0].photoAbilityLevels[
-                        data.abilities[0].photoAbilityLevels.length - 1
-                    ].level
-                }
-                value={level}
-                label={(v) => `Level ${v}`}
-                onChange={setLevel}
-                aria-label={'Level'}
-                step={5}
-            />
+            <p>{$t('select_level')}</p>
+            <Grid>
+                <GridCol span={{ base: 7, lg: 9 }}>
+                    <Slider
+                        min={initialLevel}
+                        max={maxLevel}
+                        value={level}
+                        marks={[
+                            {
+                                value: initialLevel,
+                                label: String(initialLevel),
+                            },
+                            {
+                                value: maxLevel,
+                                label: String(maxLevel),
+                            },
+                        ]}
+                        label={(v) => `Level ${v}`}
+                        onChange={setLevel}
+                        aria-label={'Level'}
+                        step={5}
+                    />
+                </GridCol>
+                <GridCol
+                    span={{ base: 5, lg: 3 }}
+                    className="flex items-center justify-center"
+                >
+                    <div className="mr-1">Lv.</div>
+                    <div className="text-center text-2xl">{level}</div>
+                </GridCol>
+            </Grid>
+
             <div>
                 {abilities.map((item, key) => (
                     <PhotoAbility key={key} item={item} level={level} />
