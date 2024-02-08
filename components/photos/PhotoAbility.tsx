0d33b5cbc3c8b54d1d105aa155ad55309abdb18d
Outvi V <git@outv.im>
2024-02-08 23:25:09 +0800

    feat(photos): UX updates

--- components/photos/PhotoAbility.tsx
+++ components/photos/PhotoAbility.tsx
@@ -15,22 +15,26 @@ const PhotoAbility = ({
     const $tpat = useTranslations('photo_ability_type')
     const { name, description, abilityType, photoAbilityLevels } = item
 
-    const currentValue = photoAbilityLevels.find((x) => x.level === level)
-        ?.value
+    const currentValue =
+        photoAbilityLevels.find((x) => x.level === level)?.value ??
+        // TODO: is the logic correct?
+        photoAbilityLevels[photoAbilityLevels.length - 1].value
 
     return (
         <div lang="ja">
             <Grid>
-                <GridCol span={9}>
+                <GridCol span={{ base: 7, lg: 9 }}>
                     <span>
                         {$tpat(PhotoAbilityType[abilityType])} <b>{name}</b>
                     </span>
                     <p>{description.replace('{0}', String(currentValue))}</p>
                 </GridCol>
-                <GridCol span={3} className="flex items-center justify-center">
-                    <div className="text-center border-sky-500 border-solid p-2">
-                        <span className="text-3xl">{currentValue}</span> <br />
-                        <span>Lv. {level}</span>
+                <GridCol
+                    span={{ base: 5, lg: 3 }}
+                    className="flex items-center justify-center"
+                >
+                    <div className="text-center text-3xl border-sky-500 border-solid p-2">
+                        {currentValue}
                     </div>
                 </GridCol>
             </Grid>
