02e753969f982cfa8f85e1c6cf773f7c5d5092a7
Outvi V <git@outv.im>
2024-02-25 22:22:39 +0800

    fix(middleware): make matcher safer

--- middleware.ts
+++ middleware.ts
@@ -11,5 +11,30 @@ export default createMiddleware({
 })
 
 export const config = {
-    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+    // Match only internationalized pathnames
+    matcher: [
+        '/',
+        '/(en|zh-Hans|ko)/:path*',
+
+        // old paths that may have non-i18n-prefixed references
+        '/about/:path*',
+        '/card/:path*',
+        '/cards/:path*',
+        '/characters/:path*',
+        '/colors/:path*',
+        '/diary/:path*',
+        '/emblems/:path*',
+        '/eventstories/:path*',
+        '/messages/:path*',
+        '/moshikoi/:path*',
+        '/mtalk/:path*',
+        '/notemap/:path*',
+        '/page/:path*.tsx',
+        '/search/:path*',
+        '/settings/:path*',
+        '/spine/:path*',
+        '/stories/:path*',
+        '/story/:path*',
+        '/units/:path*',
+    ],
 }
