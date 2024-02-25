27647af3c9fb09eefbb56cf05e11be9b224b768b
Outvi V <git@outv.im>
2024-02-25 22:18:22 +0800

    fix(i18n): validate locale

--- i18n.ts
+++ i18n.ts
@@ -1,10 +1,14 @@
 import { getRequestConfig } from 'next-intl/server'
+import { notFound } from 'next/navigation'
 
 import type { LocaleObject } from '#locales/localeData'
 import localeData from '#locales/localeData'
 import getMessageFallback from '#utils/getMessageFallback'
+import locales from '#locales/locales.json'
 
 export default getRequestConfig(async ({ locale }) => {
+    if (!locales.includes(locale as (typeof locales)[number])) notFound()
+
     return {
         messages: (localeData as Record<string, LocaleObject>)?.[locale] ?? {},
         getMessageFallback: getMessageFallback,
