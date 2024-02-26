591ef4d60f14d247c662595ab7a190793d604981
Outvi V <git@outv.im>
2024-02-27 01:06:51 +0800

    feat(card/[slug]/opengraph-image): some tweaks, thx sld

--- app/[locale]/card/[slug]/opengraph-image.tsx
+++ app/[locale]/card/[slug]/opengraph-image.tsx
@@ -33,57 +33,30 @@ export default async function OGImage({
     const cardMeta = cardResults[0]
     const { name, description, assetId, characterId } = cardMeta
 
-    const thumbIconSlug = getAssetSlug(assetId, 'thumb', true)
     const fullIconSlug = getAssetSlug(assetId, 'full', true)
 
     return new ImageResponse(
         (
             <div tw="w-full h-full flex flex-col">
-                <img
-                    width={(500 * 16) / 9}
-                    height={500}
-                    tw="rounded-lg absolute top-[100px] left-[156px] opacity-30"
-                    src={Paths.assetsImg(fullIconSlug) + '.webp'}
-                    alt={'asset full image'}
-                />
-                <div tw="flex h-1/6 w-full items-center">
+                <div tw="flex h-[80px] w-full items-center">
                     <img tw="w-12 h-12 mx-4" src={siteLogo} alt="site logo" />
                     <span tw="text-3xl">
                         info-pride /{' '}
                         <span tw="ml-3 text-3xl text-neutral-500">Card</span>
                     </span>
                 </div>
-                <div tw="flex flex-row grow justify-center">
-                    <div tw="flex items-center justify-center mr-6">
-                        <img
-                            width={144}
-                            height={144}
-                            tw="rounded-lg border-4 border-blue-600"
-                            src={Paths.assetsImg(thumbIconSlug) + '.webp'}
-                            alt={'asset thumb image'}
-                        />
-                    </div>
-                    <div tw="flex flex-col justify-center">
-                        <div tw="flex flex-col rounded-md bg-blue-300 bg-opacity-80 px-4 py-2">
-                            <div tw="flex text-6xl mb-2">{name}</div>
-                            <div tw="flex text-4xl mb-5">
-                                {jaVChr[characterId] ?? characterId}
-                            </div>
-                            <div tw="flex flex-col text-2xl text-neutral-600">
-                                {description.split('\n').map((line, key) => (
-                                    <div key={key} tw="flex flex-row">
-                                        {line}
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <img
+                    width={864}
+                    height={486}
+                    tw="rounded-lg absolute bottom-[10px] left-[10px]"
+                    src={Paths.assetsImg(fullIconSlug) + '.webp'}
+                    alt={'asset full image'}
+                />
             </div>
         ),
         {
-            width: 1200,
-            height: 600,
+            width: 884,
+            height: 575,
         },
     )
 }
