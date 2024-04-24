27e0ec41f167d89881c701c93100f9c409184581
Outvi V <git@outv.im>
2024-04-24 21:37:23 +0800

    chore(data/stories): Big4 51-55

--- data/stories.ts
+++ data/stories.ts
@@ -18,7 +18,7 @@ export const Episodes: Record<SeriesName, number[]> = {
     // adv_main_02_
     Tokyo: [25, 44],
     // adv_main_03_
-    Big4: [50],
+    Big4: [55],
     // adv_group_tri_
     TRINITYAiLE: [30],
     // adv_group_liz_
