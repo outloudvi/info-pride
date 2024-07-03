ce534422af865f2d74ce8e2d0fb0b2cc9d0af50f
Outvi V <git@outv.im>
2024-07-03 23:23:07 +0800

    feat(data/stories): story episode update

--- data/stories.ts
+++ data/stories.ts
@@ -14,21 +14,21 @@ export type SeriesName = (typeof Series)[number]
 
 export const Episodes: Record<SeriesName, number[]> = {
     // adv_main_01_
-    Hoshimi: [28, 45, 25, 24],
+    Hoshimi: [28, 45, 25, 24], // FIN
     // adv_main_02_
-    Tokyo: [25, 44],
+    Tokyo: [25, 44], // FIN
     // adv_main_03_
-    Big4: [55],
+    Big4: [66], // FIN
     // adv_group_tri_
-    TRINITYAiLE: [30],
+    TRINITYAiLE: [30], // FIN
     // adv_group_liz_
-    LizNoir: [30],
+    LizNoir: [30], // FIN
     // adv_group_mna_
-    Mana: [15],
+    Mana: [15], // FIN
     // adv_group_thrx_
     ThreeX: [20],
     // adv_group_moon_
-    Tsuki: [5, 5, 5],
+    Tsuki: [5, 5, 5, 5, 5], // FIN
     // adv_group_sun
     Sunny: [5, 5, 5],
 }
