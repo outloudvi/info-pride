3a5764d87be16535d7ed47dd9d180582d225d6da
Outvi V <git@outv.im>
2025-02-26 23:21:43 +0800

    feat: (c) for Haruhi cards

--- app/[locale]/card/[slug]/opengraph-image.tsx
+++ app/[locale]/card/[slug]/opengraph-image.tsx
@@ -1,12 +1,10 @@
-// @ts-nocheck - [TODO] ts checking fixes
-
 import { notFound } from 'next/navigation'
 import { ImageResponse } from 'next/og'
 
 import { fetchApi } from '#utils/fetchApi'
 import { getAssetSlug } from '#components/cards/cardHelper'
+import getImageCopyrightMarks from '#data/getImageCopyrightMarks'
 import Paths from '#utils/paths'
-import jaVChr from '#locales/ja/v-chr.json'
 
 export const runtime = 'edge'
 
@@ -40,35 +38,23 @@ export default async function OGImage({
 
     const fullIconSlug = getAssetSlug(assetId, 'full', 1)
 
-    const copyrightMark = [
-        ...(id.includes('mku') || id.includes('ymk') || id.includes('miku')
-            ? ['© CFM']
-            : []), // Crypton Future Media
-        ...(id.includes('chk') ||
-        id.includes('rik') ||
-        id.includes('yo') ||
-        id.includes('sush')
-            ? ['© 2017 PL!S']
-            : []), // Project Love Live! Superstar
-        ...(id.includes('chk') ||
-        id === 'card-ai-02-eve-01' ||
-        id.includes('kion')
-            ? ['© K-H/S']
-            : []), // K-ON!
-        '© PIP',
-    ]
+    const copyrightMarks = getImageCopyrightMarks(id)
 
     return new ImageResponse(
         (
             <div tw="w-full h-full flex flex-col">
                 <div tw="flex h-[80px] w-full items-center px-2">
-                    <img tw="w-12 h-12 mx-4" src={siteLogo} alt="site logo" />
+                    <img
+                        tw="w-12 h-12 mx-4"
+                        src={siteLogo as unknown as string}
+                        alt="site logo"
+                    />
                     <div tw="flex text-3xl grow">
                         info-pride /{' '}
                         <span tw="ml-3 text-3xl text-neutral-500">Card</span>
                     </div>
                     <div tw="flex self-end pb-1 text-md text-neutral-600">
-                        {copyrightMark.join(' ')}
+                        {copyrightMarks.reverse().join(' ')}
                     </div>
                 </div>
                 <img
---
+++ data/getImageCopyrightMarks.ts
@@ -0,0 +1,33 @@
+export default function getCopyrightMarks(id: string): string[] {
+    var ret = ['© PIP']
+    if (id.includes('mku') || id.includes('ymk') || id.includes('miku')) {
+        ret.push('© CFM') // Crypton Future Media
+        return ret
+    }
+
+    if (
+        id.includes('chk') ||
+        id.includes('rik') ||
+        id.includes('yo') ||
+        id.includes('sush')
+    ) {
+        ret.push('© 2017 PL!S') // Project Love Live! Superstar
+        return ret
+    }
+
+    if (
+        id.includes('chk') ||
+        id === 'card-ai-02-eve-01' ||
+        id.includes('kion')
+    ) {
+        ret.push('© K-H/S') // K-ON!
+        return ret
+    }
+
+    if (id.includes('hruh')) {
+        ret.push('© 2006 N·N/SOS') // The Melancholy of Haruhi Suzumiya
+        return ret
+    }
+
+    return ret
+}
