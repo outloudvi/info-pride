1b3e35d14f02d235cb37ff12a4fb91f9fbc447f6
Outvi V <git@outv.im>
2025-04-07 21:13:42 +0800

    chore: lower Sentry sampling rate

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -10,7 +10,7 @@ Sentry.init({
     dsn: getSentryDSN(),
 
     // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 0.5,
+    tracesSampleRate: 0.3,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
