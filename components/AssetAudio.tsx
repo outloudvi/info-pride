6c04a9cc0d758ae74e45db385d1942b8ef176f38
Outvi V <git@outv.im>
2025-05-03 17:05:55 +0800

    feat(AssetAudio): simplify logic

--- components/AssetAudio.tsx
+++ components/AssetAudio.tsx
@@ -15,9 +15,7 @@ const AssetAudio = ({
     atom?: string | null
     setAtom?: (update: SetStateAction<string | null>) => void
 }) => {
-    const $t = useTranslations('common')
     const aud = useRef<HTMLAudioElement | null>(null)
-    const [playReady, setPlayReady] = useState(0)
 
     useEffect(() => {
         const cur = aud.current
@@ -27,39 +25,12 @@ const AssetAudio = ({
         }
     }, [atom, aud, id])
 
-    useEffect(() => {
-        const cur = aud.current
-        if (cur === null) return
-        cur.load()
-        setPlayReady(0)
-    }, [id])
-
-    useEffect(() => {
-        const cb = () => {
-            setPlayReady(1)
-        }
-        const cur = aud.current
-        if (cur === null) return
-        cur.addEventListener('loadeddata', cb)
-        return () => {
-            cur.removeEventListener('loadeddata', cb)
-        }
-    }, [aud, setPlayReady])
-
     return (
         <div className="text-gray-500">
-            {playReady === 0 && (
-                <div className="mb-1">[{$t('audio_loading')}]</div>
-            )}
-            {playReady === -1 && (
-                <div className="mb-1">[{$t('audio_load_failed', { id })}]</div>
-            )}
             <audio
-                controls={playReady !== 0}
+                controls
                 ref={aud}
-                onError={() => {
-                    setPlayReady(-1)
-                }}
+                preload="metadata"
                 onPlay={() => {
                     setAtom?.(id)
                 }}
