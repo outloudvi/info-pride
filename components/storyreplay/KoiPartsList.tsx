f30b2820df42221c3670635b135b69eb3fe21017
Outvi V <git@outv.im>
2025-05-17 16:07:29 +0800

    feat(KoiPartsList): title

--- components/storyreplay/KoiPartsList.tsx
+++ components/storyreplay/KoiPartsList.tsx
@@ -1,4 +1,5 @@
 import { Paper, NavLink } from '@mantine/core'
+import { useTranslations } from 'next-intl'
 
 const KoiPartsList = ({
     parts,
@@ -9,6 +10,8 @@ const KoiPartsList = ({
     baseId: string
     currentPartId: string
 }) => {
+    const $t = useTranslations('storyreplay')
+
     return (
         <Paper
             shadow="xs"
@@ -16,6 +19,7 @@ const KoiPartsList = ({
             radius="md"
             className="hidden lg:block fixed -mt-1 left-2"
         >
+            <div className="text-center mb-2">{$t('Related stories')}</div>
             {Object.entries(parts).map(([partId, text], index) => (
                 <NavLink
                     key={index}
