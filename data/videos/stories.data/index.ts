3fb1a3a121a444444da8aabbce597b85c53fa6d7
Outvi V <git@outv.im>
2025-08-09 22:20:48 +0800

    fix: add zh-Hant items for stories.data

--- data/videos/stories.data/index.ts
+++ data/videos/stories.data/index.ts
@@ -1,11 +1,13 @@
 import en from './en'
 import zhHans from './zh-Hans'
+import zhHant from './zh-Hant'
 import ko from './ko'
 import type { StoriesData } from './types'
 
 const _: Record<string, StoriesData> = {
     en: en,
     'zh-Hans': zhHans,
+    'zh-Hant': zhHant,
     ko: ko,
 }
 
