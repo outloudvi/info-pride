6a63d61eb188b2f5388143585916da5accb01ba3
Outvi V <git@outv.im>
2025-11-04 09:25:11 +0800

    fix(version): fix version display

--- components/api/version.ts
+++ components/api/version.ts
@@ -1,28 +1,14 @@
-import * as cheerio from 'cheerio'
-
-const IOS_APP_PAGE = 'https://apps.apple.com/jp/app/id1535925293'
-
 export async function getVersion(): Promise<{
     releaseDate: string
-    releaseNotes: string
-    releaseTimestamp: string
     versionDisplay: string
 } | null> {
-    const html = await fetch(IOS_APP_PAGE).then((x) => x.text())
+    const json = await fetch(
+        'https://itunes.apple.com/lookup?id=1535925293&country=jp',
+    ).then((x) => x.json())
 
-    const $ = cheerio.load(html)
-    const shoeboxMeta = $('#shoebox-media-api-cache-apps').html()
-    if (!shoeboxMeta) return null
-    const obj = JSON.parse(shoeboxMeta)
-    for (const i of Object.values(obj)) {
-        try {
-            const item = JSON.parse(i as string)
-            const ver =
-                item.d[0].attributes.platformAttributes.ios.versionHistory[0]
-            return ver
-        } catch (_) {
-            continue
-        }
+    const result = json.results[0]
+    return {
+        versionDisplay: result.version,
+        releaseDate: result.currentVersionReleaseDate,
     }
-    return null
 }
