e584811e282fc290230ba604bfe5a65061797909
Outvi V <git@outv.im>
2026-01-04 22:20:48 +0800

    chore: move OG to Node.js runtime

--- app/[locale]/card/[slug]/opengraph-image.tsx
+++ app/[locale]/card/[slug]/opengraph-image.tsx
@@ -1,78 +1,81 @@
 import { notFound } from 'next/navigation'
 import { ImageResponse } from 'next/og'
+import { readFile } from 'node:fs/promises'
 
 import { fetchApi } from '#utils/fetchApi'
 import { getAssetSlug } from '#components/cards/cardHelper'
 import getImageCopyrightMarks from '#data/getImageCopyrightMarks'
 import Paths from '#utils/paths'
-
-export const runtime = 'edge'
+import { join } from 'node:path'
 
 export default async function OGImage({
     params: { slug },
 }: {
     params: { slug: string }
 }) {
-    const [cardResults, fontData] = await Promise.all([
+    const [cardResults, fontBuffer] = await Promise.all([
         fetchApi('Card', {
             id: slug,
         }),
-        fetch(
-            new URL('../../../../assets/DMSans-Regular.ttf', import.meta.url),
-        ).then((x) => x.arrayBuffer()),
+        readFile(join(process.cwd(), 'public/fonts/DMSans-Regular.ttf')),
     ])
 
     if (cardResults.length === 0) {
         notFound()
     }
 
-    const siteLogo = await fetch(
-        new URL(
-            '../../../../public/android-chrome-512x512.png',
-            import.meta.url,
-        ),
-    ).then((x) => x.arrayBuffer())
+    const siteLogoBuf = await readFile(
+        join(process.cwd(), 'public/android-chrome-512x512.png'),
+    ).then((x) => Uint8Array.from(x).buffer)
 
     const cardMeta = cardResults[0]
     const { id, assetId } = cardMeta
 
     const fullIconSlug = getAssetSlug(assetId, 'full', 1)
+    const imageBuf = await fetch(Paths.assetsImg(fullIconSlug) + '.png').then(
+        (x) => {
+            const contentType = x.headers.get('Content-Type')
+            if (contentType?.toLowerCase() !== 'image/png') {
+                throw new Error(`Invalid image Content-Type: ${contentType}`)
+            }
+            return x.arrayBuffer()
+        },
+    )
 
     const copyrightMarks = getImageCopyrightMarks(id)
 
     return new ImageResponse(
-        (
-            <div tw="w-full h-full flex flex-col">
-                <div tw="flex h-[80px] w-full items-center px-2">
-                    <img
-                        tw="w-12 h-12 mx-4"
-                        src={siteLogo as unknown as string}
-                        alt="site logo"
-                    />
-                    <div tw="flex text-3xl grow">
-                        info-pride /{' '}
-                        <span tw="ml-3 text-3xl text-neutral-500">Card</span>
-                    </div>
-                    <div tw="flex self-end pb-1 text-md text-neutral-600">
-                        {copyrightMarks.reverse().join(' ')}
-                    </div>
+        <div tw="w-full h-full flex flex-col">
+            <div tw="flex h-[80px] w-full items-center px-2">
+                {/* @ts-expect-error */}
+                <img tw="w-12 h-12 mx-4" src={siteLogoBuf} alt="site logo" />
+                <div tw="flex text-3xl grow">
+                    info-pride /{' '}
+                    <span tw="ml-3 text-3xl text-neutral-500">Card</span>
                 </div>
-                <img
-                    width={864}
-                    height={486}
-                    tw="rounded-lg absolute bottom-[10px] left-[10px]"
-                    src={Paths.assetsImg(fullIconSlug) + '.webp'}
-                    alt={'asset full image'}
-                />
+                <div tw="flex self-end pb-1 text-md text-neutral-600">
+                    {copyrightMarks.reverse().join(' ')}
+                </div>
             </div>
-        ),
+            <img
+                width={864}
+                height={486}
+                tw="rounded-lg absolute bottom-[10px] left-[10px]"
+                // @ts-expect-error
+                src={imageBuf}
+                alt={'asset full image'}
+            />
+        </div>,
         {
             width: 884,
             height: 575,
             fonts: [
                 {
                     name: 'DMSans',
-                    data: fontData,
+                    data: fontBuffer.buffer.slice(
+                        fontBuffer.byteOffset,
+                        fontBuffer.byteOffset + fontBuffer.byteLength,
+                    ),
                     style: 'normal',
                 },
             ],
--- app/[locale]/card/[slug]/twitter-image.tsx
+++ app/[locale]/card/[slug]/twitter-image.tsx
@@ -1,5 +1,3 @@
 import OGImage from './opengraph-image'
 
-export const runtime = 'edge'
-
 export default OGImage
